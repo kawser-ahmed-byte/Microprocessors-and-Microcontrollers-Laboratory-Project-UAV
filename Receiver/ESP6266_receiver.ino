bfbc4fdae43bd5059093fa43fccf7a6f5e877a3a
Kawser Ahmed <126769837+kawser-ahmed-byte@users.noreply.github.com>
2024-02-06 20:40:02 +0600

    Update ESP6266_receiver.ino

--- Receiver/ESP6266_receiver.ino
+++ Receiver/ESP6266_receiver.ino
@@ -91,7 +91,7 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
 }
 
 // Callback when data is received
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+  void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&incomingCommands, incomingData, sizeof(incomingCommands));
    //print no. of bytes received
   // Serial.print("Bytes received: ");
